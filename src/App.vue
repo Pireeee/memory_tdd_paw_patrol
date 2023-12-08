03a6a22cbdb9a497e89d86c75de25d3153375fac
Pire <p.clement01@orange.fr>
2023-12-08 10:43:41 +0100

    working together
    Co-authored-by: Raphael Choquet <Rafien@users.noreply.github.com>
    Co-authored-by: maximebourre <maximebourre@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,57 @@
 <script setup>
-import Memory from './components/Memory.vue';
-
-
+import Memory from './components/MemoryGame.vue';
 </script>
 
 <template>
-  <header>
-    <h1>Memory game</h1>
-  </header>
-
-  <main>
-    <Memory />
-  </main>
+    <header>
+      <div class="header">
+        <img src="/images/Paw-Patrol-Logo.png" alt="Vue logo" class="logo"/>
+        <h1>Your Memory Game</h1>
+        <p>Enjoy playing the memory game!</p>
+        
+      </div>
+    </header>
+    <main>
+      <Memory />
+    </main>
+    
 </template>
 
 <style scoped>
-  header {
-    text-align: left;
-  }
+  
+/* styles.css (or your preferred stylesheet) */
+
+html,body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  border:none;
+}
+
+.header {
+  background-color: #3498db;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+  width: 100%; /* Make the header take up the full width */
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+}
+
+.header h1 {
+  margin: 0;
+  font-size: 2em;
+}
+
+.header p {
+  margin-top: 5px;
+  font-size: 1.2em;
+}
+.logo {
+  height: 10em;
+  margin-right: 0.5em;
+  vertical-align: middle;
+}
+
+
 </style>
---
+++ src/components/MemoryGame.vue
@@ -0,0 +1,73 @@
+<script setup>
+
+const tableauImg = ["marcus.png", "chase.png", "everest.png", "rocky.png", "zuma.png", "stella.png", "ruben.png","ryder.png"]
+const imgBack = "backcard.png"
+function createTab(largeur, hauteur, tableauImg) {
+
+for (let x = 0;x<hauteur;x++){
+  for (let y = 0; y< largeur; y++){
+    createCard(tableauImg[largeur+hauteur])
+  }
+}
+} 
+
+function createCard(image) {
+  document.createElement('img', image)
+}
+
+function flipCard(image) {
+  image.classList.toggle('flipped');
+  
+}
+</script>
+
+<template>
+  <main>
+    
+    <h2>Memory game grid</h2>
+    <div class="list">
+      <article v-for="(image, imageIndex) in tableauImg" :key="imageIndex" v-on:click="flipCard(image)">
+        <img :src="'/images/'+image" :alt="image">
+      </article>
+    </div>
+    
+    
+  </main>
+</template>
+<style scoped>
+.list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-gap: 10px;
+  width: 100%;
+  max-width: 510px;
+}
+
+article {
+  height: 150px;
+  background-color: #efefef;
+  text-align: center;
+  text-transform: capitalize;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
+
+  /* justify-content: center;
+  align-content: center; */
+  
+}
+
+h3 {
+  margin: 0;
+}
+
+img {
+  width: 120px;
+  height: 120px;
+
+}
+
+img.flipped {
+  transform: rotateY(180deg);
+}
+</style>
