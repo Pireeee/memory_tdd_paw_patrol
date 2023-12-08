6509a354e7f2435be30fe6061b9c1d64508b2c73
Pire <p.clement01@orange.fr>
2023-12-08 09:12:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Memory from './components/Memory.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <h1>Memory game</h1>
+  </header>
+
+  <main>
+    <Memory />
+  </main>
+</template>
+
+<style scoped>
+  header {
+    text-align: left;
+  }
+</style>
---
+++ src/components/Memory.vue
@@ -0,0 +1,34 @@
+<script setup>
+const tableauImg = ["/public/images/Paw-Patrol-Logo.png", ""]
+function createTab(largeur, hauteur, tableauImg) {
+
+for (let x = 0;x<hauteur;x++){
+  for (let y = 0; y< largeur; y++){
+    createCard(tableauImg[largeur+hauteur])
+  }
+}
+}
+
+function createCard(image) {
+  document.createElement('img', image)
+}
+</script>
+
+<template>
+  <main>
+    <h2>Memory game grid</h2>
+    <ul>
+      <li v-for="image in tableauImg" :key="">
+        <diimageIndexv>
+        <img :src="image">
+      </div>
+      </li>
+    </ul>
+    
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
